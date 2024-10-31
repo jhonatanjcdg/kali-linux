5e59a589a7dc6b54298d392d02710724649bac10
jhonatanjcdg <j3172224877@gmail.com>
2024-10-31 09:18:50 -0500

    Navbar Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbarlogo">
-        <Link to="/">Kali linux</Link>
+        <Link to="/">Kali Linux</Link>
       </div>
-      <ul className="navbarlinks">
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span className="menu-icon-bar"></span>
+        <span className="menu-icon-bar"></span>
+        <span className="menu-icon-bar"></span>
+      </div>
+      <ul className={`navbarlinks ${isOpen ? 'navbarlinks-open' : ''}`}>
         <li><Link to="/">Inicio</Link></li>
-        <li><Link to='/installKali'>Tutorial</Link></li>
+        <li><Link to="/installKali">Tutorial</Link></li>
         <li><Link to="/history">Historia</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/content">Contenido</Link></li>
