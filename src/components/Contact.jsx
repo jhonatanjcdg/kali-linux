64ba5aa5a566294786edc2b29308efe323d6c53b
jhonatanjcdg <j3172224877@gmail.com>
2024-10-31 17:02:34 -0500

    cambio la dirección del back

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://back-kali-linux.vercel.app/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
